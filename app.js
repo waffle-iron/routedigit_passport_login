fa37bbd9ddc093a51990786d83b70e41e92b4cae
Bartholomew Krystman <bkrystman@protonmail.ch>
2016-01-03 11:29:06 -0700

    routes added to files

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var dashboard = require('./routes/dashboard');
+var login = require('./routes/login');
+var register = require('./routes/register');
+
 
 var app = express();
 
@@ -24,6 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/register', register);
+app.use('/dashboard', dashboard);
+app.use('/login', login);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
-});
-
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Express' });
-});
-
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', { title: 'Express' });
-});
-
 module.exports = router;
