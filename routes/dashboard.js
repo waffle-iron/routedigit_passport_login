906e35c88c462b91e51976797f6c2fe55c680201
Bartholomew Krystman <bkrystman@protonmail.ch>
2017-01-14 15:34:26 -0700

    personal user message added to dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,9 @@ var queries = require('../db/queries');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Express' });
+  queries.getSingleUser(req.user).then((data)=>{
+    res.render('dashboard', { title: data[0].fullName });
+  })
 });
 
 module.exports = router;
