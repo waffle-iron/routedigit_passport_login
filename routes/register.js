aac59171c5fd0f63f24da44ffb02e30deb42e1d3
Bartholomew Krystman <bkrystman@protonmail.ch>
2017-01-15 11:41:27 -0700

    user pictures are added based on male/female

--- routes/register.js
+++ routes/register.js
@@ -9,14 +9,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-  got('https://randomuser.me/api')
+  var regname = req.body.regname;
+  var regemail = req.body.regemail;
+  var regpassword = req.body.regpassword;
+  var regender = req.body.regender;
+  got('https://randomuser.me/api/?gender='+regender)
     .then(response => {
         var data = response.body;
         data = JSON.parse(data);
-        var regname = req.body.regname;
-        var regemail = req.body.regemail;
-        var regpassword = req.body.regpassword;
-        var regender = req.body.regender;
         var regimage = data.results[0].picture.large;
         if(regname.length>0 && regemail.length>0 && regpassword.length>0 && regender.length>0 && regimage.length>0){
           queries.addNewUser(regname, regemail, regpassword, regender, regimage)
