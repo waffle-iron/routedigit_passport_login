e6d050e2c3a1ccd7f0bbd34c6dba705bdb05ac85
Bartholomew Krystman <bkrystman@protonmail.ch>
2017-01-14 20:30:04 -0700

    some changes added

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
-// Update with your config settings.
-
+"use strict"
+require('dotenv').config();
 module.exports = {
 
   development: {
@@ -8,18 +8,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
